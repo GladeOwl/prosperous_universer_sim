e332d0ec981fccdcaa3692ac456981395b2b75a1
GladeOwl <owloftheglade@gmail.com>
2022-09-13 15:55:49 +0530

    idea_start

--- production.py
+++ production.py
@@ -5,13 +5,20 @@ from inventory import Inventory
 
 
 class Producer:
-    def __init__(self, name: string, queue: list, inventory: Inventory) -> None:
+    def __init__(
+        self, name: string, queue: list, queue_slots: int, inventory: Inventory
+    ) -> None:
         self.name: string = name
         self.queue: list = queue
+        self.queue_slots = queue_slots
         self.inventory: Inventory = inventory
-        self.current_production_index: int = 0
-        self.current_production: Item = self.queue[self.current_production_index]
-        self.current_time: int = self.current_production.time
+        self.current_production = []
+
+    def setup_production(self):
+        for index in range(self.queue_slots - len(self.current_production)):
+            production = self.queue.pop(0)
+            self.current_production.append(production)
+            self.queue.append(production)
 
     def tick(self, time: tuple):
         """Ticks the production by 1"""
@@ -49,3 +56,26 @@ class Producer:
         self.inventory.add_stock(
             self.current_production, self.current_production.produced_per_cycle, time
         )
+
+
+class Production:
+    def __init__(self, item: Item, time: int, inventory: Inventory) -> None:
+        self.item: Item = item
+        self.time: int = time
+        self.time_left = time
+        self.inventory: Inventory = inventory
+
+    def tick(self):
+        self.time_left -= 1
+        if self.time_left <= 0:
+            pass
+
+    def finish_production(self):
+        pass
+
+    def withdraw_resources(self):
+        for item in self.item.reciepe:
+            self.inventory.remove_stock(item["item"], item["amount"])
+
+    def deposit_resources(self):
+        self.inventory.add_stock(self.item, self.item.produced_per_cycle)
